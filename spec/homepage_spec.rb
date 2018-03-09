54ede55072108fca3c038ab5b9b8c1fd2d55fdbd
Andrew Myers <afredmyers@gmail.com>
2018-03-09 16:26:14 -0700

    Adds rspec and other gems for testing
    * Adds boiler plate RSpec code.
    * Adds config to get Capybara to run and Jekyll to build site.
    * Adds spec to ping homepage as sanity check.

---
+++ spec/homepage_spec.rb
@@ -0,0 +1,6 @@
+describe "homepage", type: :feature, js: true do
+  it "is accessible" do
+    visit '/'
+    expect(find('h1.post-title-main').text).to match /A Guide For The Perplexed Samvera Developer/
+  end
+end
