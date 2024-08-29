ddd758f15db488c87487e8edf87829e8b3d08736
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2024-08-28 21:04:28 -0700

    update homepage spec

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe "homepage", type: :feature, js: true do
-  it "is accessible" do
+describe 'homepage', type: :feature, js: true do
+  it 'is accessible' do
     visit '/'
-    expect(find('h1.post-title-main').text).to match /A Guide For The Samvera Community/
+    expect(find('h1.page-header').text).to match(/This site has been sunset/)
   end
 end
